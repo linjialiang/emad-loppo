e9dee7ff247dd0cdc0a4283eab4f45ceb6d3720e
madnesslin <linjialiang@163.com>
2021-09-18 23:57:12 +0800

    修正主题名称错误

--- utils/themePath.js
+++ utils/themePath.js
@@ -3,10 +3,10 @@ const path = require('path');
 function themePath(themeName) {
   let theme_src = '';
   try {
-    const modulePath = path.resolve(process.cwd(), 'node_modules', 'loppo-theme-' + themeName);
+    const modulePath = path.resolve(process.cwd(), 'node_modules', 'emad-loppo-theme-' + themeName);
     theme_src = require(modulePath);
   } catch (e) {
-    theme_src = require(path.resolve(__dirname, '../node_modules', 'loppo-theme-' + themeName));
+    theme_src = require(path.resolve(__dirname, '../node_modules', 'emad-loppo-theme-' + themeName));
   }
   return theme_src;
 }
